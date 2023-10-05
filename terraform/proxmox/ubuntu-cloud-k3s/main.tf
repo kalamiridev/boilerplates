8b4c4d7eac7ae2cc9c9b7c9d2fde2f34aab1fe84
Sasa Vlaisavljevic <info@team4rent.com>
2023-10-05 10:08:20 +0200

    tarraform ubuntu-cloud k3s

---
+++ terraform/proxmox/ubuntu-cloud-k3s/main.tf
@@ -0,0 +1,56 @@
+# https://registry.terraform.io/providers/wearespindle/proxmox/latest/docs
+terraform {
+    required_version = ">= 0.13.0"
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "2.9.8"
+        }
+    }
+}
+
+# Proxmox Provider Creditials
+provider "proxmox" {
+    pm_api_url = var.proxmox_api_url
+    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_token_secret = var.proxmox_api_token_secret
+    
+    # (Optional) Skip TLS Verification
+    pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "k3s" {
+  count = 4
+  name = "k3s-0${count.index + 1}"
+  target_node = var.proxmox_node
+  vmid = "30${count.index + 1}"
+  clone = "ubuntu-cloud"
+  agent = 1
+  os_type = "cloud-init"
+  cores = 1
+  sockets = 1
+  cpu = "host"
+  memory = 4096
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot = 0
+    size = "10G"
+    type = "scsi"
+    storage = "local-lvm"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network
+    ]
+  }
+  ipconfig0 = "ip=192.168.99.3${count.index + 1}/24,gw=192.168.99.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
