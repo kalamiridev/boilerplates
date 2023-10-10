a1155372fc94c13513ade030a54b3fdc6e761f95
kalamiridev <svlais@gmail.com>
2023-10-10 12:39:36 +0200

    Proxmox cloud machines

--- terraform/proxmox/debian-cloud-media/main.tf
+++ terraform/proxmox/debian-cloud-media/main.tf
@@ -19,23 +19,26 @@ provider "proxmox" {
     pm_tls_insecure = true
 }
 
-resource "proxmox_vm_qemu" "k3s" {
-  count = 2
-  name = "media-k3s-0${count.index + 1}"
+resource "proxmox_vm_qemu" "media" {
+  count = 1
+  name = "media-0${count.index + 1}"
   target_node = var.proxmox_node
-  vmid = "60${count.index + 1}"
-  clone = "debian-cloud"
+  vmid = "40${count.index + 1}"
+  clone = "debian12-cloud"
   agent = 1
   os_type = "cloud-init"
   cores = 1
   sockets = 1
   cpu = "host"
-  memory = 4096
+  memory = 2048
+  balloon = 0
   scsihw = "virtio-scsi-pci"
   bootdisk = "scsi0"
+  onboot = true
+  oncreate = false
   disk {
     slot = 0
-    size = "32G"
+    size = "16G"
     type = "scsi"
     storage = "local-lvm"
     iothread = 1
@@ -49,7 +52,7 @@ resource "proxmox_vm_qemu" "k3s" {
       network
     ]
   }
-  ipconfig0 = "ip=192.168.99.6${count.index + 1}/24,gw=192.168.99.1"
+  ipconfig0 = "ip=192.168.99.4${count.index + 1}/24,gw=192.168.99.1"
   sshkeys = <<EOF
   ${var.ssh_key}
   EOF
--- terraform/proxmox/ubuntu-cloud-tools/main.tf
+++ terraform/proxmox/ubuntu-cloud-tools/main.tf
@@ -56,3 +56,42 @@ resource "proxmox_vm_qemu" "tools" {
   ${var.ssh_key}
   EOF
 }
+
+resource "proxmox_vm_qemu" "k3s" {
+  count = 3
+  name = "k3s-0${count.index + 1}"
+  target_node = var.proxmox_node
+  vmid = "60${count.index + 1}"
+  clone = "ubuntu22.04-cloud"
+  agent = 1
+  os_type = "cloud-init"
+  cores = 1
+  sockets = 1
+  cpu = "host"
+  memory = 4096
+  balloon = 0
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  onboot = true
+  oncreate = false
+  disk {
+    slot = 0
+    size = "32G"
+    type = "scsi"
+    storage = "local-lvm"
+    iothread = 1
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  lifecycle {
+    ignore_changes = [
+      network
+    ]
+  }
+  ipconfig0 = "ip=192.168.99.6${count.index + 1}/24,gw=192.168.99.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
