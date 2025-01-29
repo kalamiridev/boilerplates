939cfed71cd6c1d13b109676270b7490a408d449
kalamiridev <svlais@gmail.com>
2025-01-29 14:56:01 +0100

    schema for pgvector

---
+++ docker-compose/pgvector/schema.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Create sample table
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    item_data JSONB,
+    embedding vector(1536) -- vector data
+);
