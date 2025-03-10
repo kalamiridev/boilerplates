db6b73e9f4dd52d6648085cbde95c84c5b6c2dbf
kalamiridev <svlais@gmail.com>
2025-03-10 13:14:27 +0100

    Docker compose removed

--- docker-compose/pgvector/postgres/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Create sample table
-CREATE TABLE items (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    item_data JSONB,
-    embedding vector(1536) -- vector data
-);
